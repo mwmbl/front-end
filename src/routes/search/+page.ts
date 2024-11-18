ced7d79ab922430e9df2d985d7fe47df78970018
xyny <git@xyny.anonaddy.me>
2024-11-18 18:00:55 +0200

    feat: add basic scaffolding for wasm ranker integration
    commented out to not cause build issues, as the ranker package is not yet available on the production server

--- src/routes/search/+page.ts
+++ src/routes/search/+page.ts
@@ -1,4 +1,12 @@
+// add the ranker to dependencies to use wasm ranker ("ranker": "file:./pkg/" when testing)
+
+// uncomment to use wasm ranker
+// export const ssr = false;
+
 export async function load({ url }) {
+	// const useWasmRanker = url.searchParams.get('useWasmRanker') === 'true';
+
+	// if (!useWasmRanker) {
 	const response = await fetch('https://mwmbl.org/search/?s=' + url.searchParams.get('q'));
 	const results: Array<{
 		title: Array<{ value: string; is_bold: boolean }>;
@@ -11,4 +19,49 @@ export async function load({ url }) {
 		query: url.searchParams.get('q') as string | undefined,
 		results: results
 	};
+	// }
+	// else {
+	// 	const wasm = await import('ranker');
+
+	// 	const query = url.searchParams.get('q');
+	// 	const ranker = wasm.Ranker.new(query ?? '');
+	// 	const terms = ranker.get_query_terms();
+	// 	for (const term of terms) {
+	// 		const res = await fetch(`https://api.mwmbl.org/api/v1/search/raw?s=${term}`);
+	// 		const json = await res.json();
+	// 		for (const result of json.results) {
+	// 			ranker.add_search_result(result.url, result.title, result.extract);
+	// 		}
+	// 	}
+	// 	const rankedData = ranker.rank();
+
+	// 	const results: Array<{
+	// 		title: Array<{ value: string; is_bold: boolean }>;
+	// 		extract: Array<{ value: string; is_bold: boolean }>;
+	// 		url: string;
+	// 		source: string;
+	// 	}> = await rankedData.map((result: { url: string; title: string; extract: string }) => {
+	// 		return {
+	// 			title: result.title.split(' ').map((word: string) => {
+	// 				return {
+	// 					value: word + ' ',
+	// 					is_bold: query?.split(' ').some((q) => q.toLowerCase() === word.toLowerCase())
+	// 				};
+	// 			}),
+	// 			extract: result.extract.split(' ').map((word: string) => {
+	// 				return {
+	// 					value: word + ' ',
+	// 					is_bold: query?.split(' ').some((q) => q.toLowerCase() === word.toLowerCase())
+	// 				};
+	// 			}),
+	// 			url: result.url,
+	// 			source: ''
+	// 		};
+	// 	});
+
+	// 	return {
+	// 		query: url.searchParams.get('q') as string | undefined,
+	// 		results: results
+	// 	};
+	// }
 }

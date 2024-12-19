726af49c07a0b318fa29421740ce365e422a5404
xyny <git@xyny.anonaddy.me>
2024-12-19 20:05:27 +0200

    feat(domain-submissions): allow filtering by status

--- src/routes/(nosearch)/domain-submissions/+page.svelte
+++ src/routes/(nosearch)/domain-submissions/+page.svelte
@@ -6,9 +6,12 @@
 	import RiArrowDropRightLine from '~icons/ri/arrow-drop-right-line';
 	import RiLinksLine from '~icons/ri/links-line';
 	import RiLoader2Line from '~icons/ri/loader-2-line';
+
 	import type { SubmissionsResult } from './+page.server.js';
 	import SignInButton from '@/components/custom/SignInButton.svelte';
+
 	import * as Card from '@/components/ui/card';
+	import * as Select from '$lib/components/ui/select';
 
 	let { data } = $props();
 
@@ -37,37 +40,45 @@
 		clearTimeout(debounceTimer);
 		debounceTimer = setTimeout(callback, ms);
 	}
+
+	let filter = $state({
+		status: { value: '', label: 'any' }
+	});
 </script>
 
-{#snippet submissions(submissions: SubmissionsResult)}
-	<ul class="grid w-fit grid-cols-[16rem,1fr,1fr] gap-2 pr-16">
-		{#each submissions.items as item}
-			<li class="contents">
-				<div class="grid grid-cols-[7rem,1fr] gap-x-2">
-					<div
-						class={`col-span-2 flex flex-col justify-center rounded-2xl p-2 px-4 text-black ${item.status == 'APPROVED' ? ' bg-green-300' : item.status == 'REJECTED' ? ' !col-span-1 bg-red-300' : ' bg-gray-300'}`}
-					>
-						{item.status}
-					</div>
-					{#if item.status == 'REJECTED'}
-						<div class="mr-2 flex flex-col">
-							<div>reason: {item.rejection_reason}</div>
-							{#if item.rejection_detail}
-								<div>detail: {item.rejection_detail}</div>
-							{/if}
+{#snippet submissions(items: SubmissionsResult['items'])}
+	{#if items.length > 0}
+		<ul class="grid w-fit grid-cols-[16rem,1fr,1fr] gap-2 pr-16">
+			{#each items as item}
+				<li class="contents">
+					<div class="grid grid-cols-[7rem,1fr] gap-x-2">
+						<div
+							class={`col-span-2 flex flex-col justify-center rounded-2xl p-2 px-4 text-black ${item.status == 'APPROVED' ? ' bg-green-300' : item.status == 'REJECTED' ? ' !col-span-1 bg-red-300' : ' bg-gray-300'}`}
+						>
+							{item.status}
 						</div>
-					{/if}
-				</div>
-				<div class="flex flex-row items-center gap-2 pr-4">
-					<span>{item.name}</span>
-				</div>
-				<div class="flex flex-row items-center gap-2">
-					{new Date(item.submitted_on).toISOString().slice(0, 10)},
-					{new Date(item.submitted_on).toISOString().slice(11, 16)} (UTC)
-				</div>
-			</li>
-		{/each}
-	</ul>
+						{#if item.status == 'REJECTED'}
+							<div class="mr-2 flex flex-col">
+								<div>reason: {item.rejection_reason}</div>
+								{#if item.rejection_detail}
+									<div>detail: {item.rejection_detail}</div>
+								{/if}
+							</div>
+						{/if}
+					</div>
+					<div class="flex flex-row items-center gap-2 pr-4">
+						<span>{item.name}</span>
+					</div>
+					<div class="flex flex-row items-center gap-2">
+						{new Date(item.submitted_on).toISOString().slice(0, 10)},
+						{new Date(item.submitted_on).toISOString().slice(11, 16)} (UTC)
+					</div>
+				</li>
+			{/each}
+		</ul>
+	{:else}
+		Nothing on this page...
+	{/if}
 {/snippet}
 
 <main class="flex w-full max-w-4xl flex-col gap-2 self-center px-6">
@@ -102,7 +113,7 @@
 				{#if domainInput.length > 0}
 					{#if submissionsForInput.count > 0}
 						<div class="mb-4">Found {submissionsForInput.count} pre-existing submission(s)</div>
-						{@render submissions(submissionsForInput)}
+						{@render submissions(submissionsForInput.items)}
 					{:else}
 						Found no pre-existing submission(s)
 					{/if}
@@ -131,7 +142,27 @@
 			{/await}
 		</div>
 	</div>
-	{@render submissions(data.submissions)}
+	<form class="mt-3 p-2">
+		<h3 class="text-lg">Filter</h3>
+		<label class="flex flex-row items-center gap-4">
+			By Status <Select.Root bind:selected={filter.status}>
+				<Select.Trigger class="w-[180px]">
+					<Select.Value placeholder="any" />
+				</Select.Trigger>
+				<Select.Content>
+					<Select.Item value="">any</Select.Item>
+					<Select.Item value="APPROVED">APPROVED</Select.Item>
+					<Select.Item value="REJECTED">REJECTED</Select.Item>
+					<Select.Item value="PENDING">PENDING</Select.Item>
+				</Select.Content>
+			</Select.Root>
+		</label>
+	</form>
+	{@render submissions(
+		data.submissions.items.filter((item) =>
+			filter.status.value !== '' ? item.status == filter.status.value : true
+		)
+	)}
 	<div class="flex flex-row items-center justify-center gap-4 p-4">
 		<Button
 			href={data.page == 0 ? '' : `?page=${data.page - 1}`}

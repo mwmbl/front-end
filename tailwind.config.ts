b41543c7545a8325aa1984e83cbe0ccc6605ea75
xyny <git@xyny.anonaddy.me>
2025-06-22 14:21:04 +0300

    chore: upgrade deps pt1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import tailwindcssAnimate from 'tailwindcss-animate';
 
 const config: Config = {
 	darkMode: ['class'],
@@ -62,12 +63,32 @@ const config: Config = {
 				sans: ['Hind Vadodara', 'sans-serif'],
 				display: ['Nunito Variable', 'sans-serif']
 			},
+			keyframes: {
+				'accordion-down': {
+					from: { height: '0' },
+					to: { height: 'var(--bits-accordion-content-height)' }
+				},
+				'accordion-up': {
+					from: { height: 'var(--bits-accordion-content-height)' },
+					to: { height: '0' }
+				},
+				'caret-blink': {
+					'0%,70%,100%': { opacity: '1' },
+					'20%,50%': { opacity: '0' }
+				}
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				'caret-blink': 'caret-blink 1.25s ease-out infinite'
+			},
 			backgroundImage: {
 				'brand-gradient':
 					'linear-gradient(in oklab 130deg, hsla(48, 100%, 61%, var(--tw-bg-opacity)), hsla(283, 100%, 77%, var(--tw-bg-opacity)), hsla(206, 100%, 73%, var(--tw-bg-opacity)))'
 			}
 		}
-	}
+	},
+	plugins: [tailwindcssAnimate]
 };
 
 export default config;

0fcadd48bac6ace19fb5fb14fce427036f744859
xyny <git@xyny.anonaddy.me>
2025-08-07 21:29:07 +0300

    feat: initial impl of listing user votes
    this majorly needs design and functionality re-dos, was done very scrappy and quick

--- src/routes/(nosearch)/account/+page.svelte
+++ src/routes/(nosearch)/account/+page.svelte
@@ -1,14 +1,36 @@
-<script>
+<script lang="ts">
 	import { Button } from '@/components/ui/button';
 	import * as Card from '@/components/ui/card';
 	import { Input } from '@/components/ui/input';
 	import * as Tabs from '@/components/ui/tabs';
 	import * as AlertDialog from '$lib/components/ui/alert-dialog';
 
+	import RiArrowUpSLine from '~icons/ri/arrow-up-s-line';
+	import RiArrowDownSLine from '~icons/ri/arrow-down-s-line';
+	import RiDeleteBin5Line from '~icons/ri/delete-bin-5-line';
+
 	import { getCookies } from '@/cookies.svelte';
 	const cookies = getCookies();
 
 	const { data } = $props();
+
+	function deleteVote(query: string, url: string) {
+		fetch('/api/v1/platform/search-results/vote', {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ url, query })
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				if (data.error) {
+					alert(data.error);
+				} else {
+					alert('Vote deleted successfully');
+				}
+			});
+	}
 </script>
 
 <main class="flex w-full max-w-2xl flex-col gap-2 self-center px-6">
@@ -120,5 +142,38 @@
 				</AlertDialog.Footer>
 			</AlertDialog.Content>
 		</AlertDialog.Root>
+
+		<hr class="my-4" />
+
+		<h3 class="text-xl">My votes</h3>
+		{#if data.votes}
+			<ul class="mt-4 flex flex-col gap-4">
+				{#each data.votes.items as vote}
+					<li class="flex flex-row gap-2 border-t-1 pt-4">
+						<div class="bg-card text-accent-text flex items-center p-2">
+							{#if vote.vote_type === 'upvote'}
+								<RiArrowUpSLine class="size-8" />
+							{:else}
+								<RiArrowDownSLine class="size-8" />
+							{/if}
+						</div>
+						<div class="flex flex-col">
+							<Button variant="link" class="font-medium" href={vote.url}>{vote.url}</Button>
+							<p class="px-4">for query: {vote.query}</p>
+						</div>
+						<Button
+							onclick={() => deleteVote(vote.query, vote.url)}
+							variant="destructive"
+							size="icon"
+							class="size-9"
+						>
+							<RiDeleteBin5Line class="size-6" />
+						</Button>
+					</li>
+				{/each}
+			</ul>
+		{:else}
+			<p>No votes yet.</p>
+		{/if}
 	{/if}
 </main>

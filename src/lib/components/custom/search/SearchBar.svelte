b9525c5b9a1cecf350327518695feb9d85df9c53
Realmy <163438634+RealmyTheMan@users.noreply.github.com>
2025-08-07 15:14:13 +0200

    feat: improve searchbar UX much

--- src/lib/components/custom/search/SearchBar.svelte
+++ src/lib/components/custom/search/SearchBar.svelte
@@ -5,21 +5,31 @@
 
 	import RiSearchLine from '~icons/ri/search-line';
 	import PhArrowSquareOutFill from '~icons/ph/arrow-square-out-fill';
-	import MaterialSymbolsCloseRounded from '~icons/material-symbols/close-rounded';
+	import RiCloseFill from '~icons/ri/close-fill';
+	import Spinner from '@/components/ui/spinner/spinner.svelte';
 
 	let { query }: { query?: string } = $props();
 
 	let input: HTMLInputElement | null = $state(null);
+	let searchBarRef: HTMLFormElement;
 
 	let searchCompletions: string[] = $state([]);
-	let completionsExist = $derived(
-		searchCompletions.length > 0 && searchCompletions[0] !== 'search: google.com '
-	);
+	let completionsVisible = $state(false);
+	let completionsLoading = $state(true);
 
 	// AbortControllers are used to cancel fetches when the user searchers for something
 	// This is to prevent the page being stuck loading completions after the user has already searched
 	let abortControllers: AbortController[] = [];
+
 	function fetchSearchCompletions(query: string | undefined) {
+		completionsLoading = searchCompletions.length === 0;
+		completionsVisible = !!(query && query.trim().length > 0);
+
+		if (!query || query.length === 0) {
+			resetCompletions();
+			return;
+		}
+
 		if (query != undefined) {
 			const controller = new AbortController();
 			const signal = controller.signal;
@@ -28,6 +38,7 @@
 				.then((res) => {
 					res.json().then((json) => {
 						searchCompletions = json[1];
+						completionsLoading = false;
 					});
 				})
 				.catch((e: Error) => {
@@ -38,7 +49,9 @@
 				});
 		}
 	}
+
 	function resetCompletions() {
+		completionsVisible = false;
 		abortControllers.forEach((controller) => controller.abort());
 		abortControllers = [];
 		searchCompletions = [];
@@ -46,28 +59,41 @@
 	}
 </script>
 
+<svelte:window
+	onclick={(e) => {
+		// If user clicks outside of search bar, reset (hide) completions
+		if (!completionsVisible || !e.target || !(e.target instanceof Node)) return;
+		if (e.target !== searchBarRef && !searchBarRef.contains(e.target)) resetCompletions();
+	}}
+/>
+
 <form
 	class="relative flex w-full max-w-184 flex-row items-center"
 	action="/search"
 	method="get"
 	onsubmit={resetCompletions}
+	bind:this={searchBarRef}
 >
 	<Input
 		type="search"
 		name="q"
 		placeholder="Search with Mwmbl..."
 		aria-label="Search with Mwmbl"
-		class={'bg-card z-20 h-12 rounded-2xl border-none px-5 text-lg ' +
-			(completionsExist ? ' rounded-b-none ' : '')}
+		class={'bg-card z-20 h-12 rounded-2xl border-none pr-12 pl-4 text-lg ' +
+			(completionsVisible ? ' rounded-b-none ' : '')}
+		autocomplete="off"
 		bind:ref={input}
 		bind:value={query}
 		oninput={() => fetchSearchCompletions(query)}
+		onclick={(e) => {
+			if (e.currentTarget.value.trim() !== '') fetchSearchCompletions(e.currentTarget.value);
+		}}
 	/>
-	{#if query == undefined || completionsExist}
+	{#if query == undefined || completionsVisible}
 		<Button
 			tabindex={-1}
 			type="submit"
-			class="bg-brand-gradient absolute right-3 z-30 h-8 w-8 rounded-full disabled:pointer-events-auto disabled:cursor-default disabled:opacity-100"
+			class="bg-brand-gradient absolute right-2 z-30 h-8 w-8 rounded-full p-0 disabled:pointer-events-auto disabled:cursor-default disabled:opacity-100"
 			disabled={query == undefined}
 			title="Search Mwmbl"
 			aria-label="Search Mwmbl"
@@ -80,47 +106,53 @@
 				query = undefined;
 				input?.focus();
 			}}
-			class="bg-brand-gradient absolute right-3 z-30 h-8 w-8 rounded-full"
+			class="bg-brand-gradient absolute right-2 z-30 h-8 w-8 rounded-full p-0"
 			title="Clear query and search again"
 			aria-label="Clear query and search again"
 		>
-			<MaterialSymbolsCloseRounded class="min-h-5 min-w-5 text-black" />
+			<RiCloseFill class="min-h-6 min-w-6 text-black" />
 		</Button>
 	{/if}
 
 	<Card.Root
 		class={'absolute top-12 z-10 flex w-[calc(100%)] rounded-t-none rounded-b-2xl border-none shadow-sm' +
-			(completionsExist ? ' px-4 pt-4 pb-4' : ' p-0')}
+			(completionsVisible ? ' min-h-16 p-3 pt-4' : ' p-0')}
 	>
 		<ol>
-			{#if completionsExist}
-				{#each searchCompletions as completion}
-					{#if !completion.match(/^.*: /)}
-						<Button variant="link" onclick={resetCompletions} href="/search?q={completion}"
-							>{completion}</Button
-						>
-					{:else if completion.startsWith('search: google.com ')}
-						<Button
-							target="_blank"
-							href="https://google.com/search?q={completion.replace(/^search: google\.com/, '')}"
-						>
-							<span>Search on Google: {completion.replace(/^search: google\.com/, '')}</span>
-							<RiSearchLine class="ml-1 min-h-5 min-w-5 text-black" />
-						</Button>
-					{:else if completion.startsWith('go: ')}
-						<Button
-							variant="secondary"
-							target="_blank"
-							href={'https://' + completion.replace(/^go: /, '')}
-						>
-							<span>Open website: {completion.replace(/^go: /, '')}</span>
-							<PhArrowSquareOutFill class="ml-1 min-h-5 min-w-5 text-black dark:text-white" />
-						</Button>
-					{/if}
-				{/each}
+			{#if completionsVisible}
+				{#if completionsLoading}
+					<div class="flex h-10 w-full items-center justify-center text-xl">
+						<Spinner />
+					</div>
+				{:else}
+					{#each searchCompletions as completion}
+						{#if !completion.match(/^.*: /)}
+							<Button variant="link" onclick={resetCompletions} href="/search?q={completion}"
+								>{completion}</Button
+							>
+						{:else if completion.startsWith('search: google.com ')}
+							<Button
+								target="_blank"
+								href="https://google.com/search?q={completion.replace(/^search: google\.com/, '')}"
+							>
+								<span>Search on Google: {completion.replace(/^search: google\.com/, '')}</span>
+								<RiSearchLine class="ml-1 min-h-5 min-w-5 text-black" />
+							</Button>
+						{:else if completion.startsWith('go: ')}
+							<Button
+								variant="secondary"
+								target="_blank"
+								href={'https://' + completion.replace(/^go: /, '')}
+							>
+								<span>Open website: {completion.replace(/^go: /, '')}</span>
+								<PhArrowSquareOutFill class="ml-1 min-h-5 min-w-5 text-black dark:text-white" />
+							</Button>
+						{/if}
+					{/each}
+				{/if}
 			{/if}
 		</ol>
-		{#if completionsExist}
+		{#if completionsVisible}
 			<Button
 				size="icon"
 				class="absolute right-3 bottom-3 ml-auto h-8 w-8 rounded-full"
@@ -129,7 +161,7 @@
 				title="Close search completions"
 				aria-label="Close search completions"
 			>
-				<MaterialSymbolsCloseRounded class="min-h-5 min-w-5 text-black dark:text-white" />
+				<RiCloseFill class="min-h-5 min-w-5 text-black dark:text-white" />
 			</Button>
 		{/if}
 	</Card.Root>
---
+++ src/lib/components/ui/spinner/spinner.svelte
@@ -0,0 +1,19 @@
+<!-- Make it spin, and scale based on font size -->
+<div class="fill-muted-foreground size-[1em] animate-spin">
+	<svg
+		width="100%"
+		height="100%"
+		viewBox="0 0 128 128"
+		version="1.1"
+		xmlns="http://www.w3.org/2000/svg"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+		xml:space="preserve"
+		style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"
+	>
+		<g transform="matrix(0.765961,0,0,0.765961,-526.562,-304.06)">
+			<path
+				d="M854.563,480.521C854.563,526.636 817.123,564.076 771.008,564.076C724.893,564.076 687.453,526.636 687.453,480.521C687.453,434.406 724.893,396.966 771.008,396.966L771.008,417.855C736.421,417.855 708.342,445.934 708.342,480.521C708.342,515.107 736.421,543.187 771.008,543.187C805.594,543.187 833.674,515.107 833.674,480.521L854.563,480.521Z"
+			/>
+		</g>
+	</svg>
+</div>

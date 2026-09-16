1efcf3b477a65559490af2f2b44c95ee8214d272
blueheron786 <blueheron786@gmail.com>
2026-09-16 12:40:42 -0400

    pnpm format

--- src/routes/(nosearch)/+page.svelte
+++ src/routes/(nosearch)/+page.svelte
@@ -6,10 +6,10 @@
 
 <main class="flex min-h-[75svh] flex-col items-center justify-center gap-9 px-8">
 	<h2
-		class="justify-self-center px-8 text-center text-4xl font-semibold leading-snug! md:text-5xl lg:text-6xl"
+		class="justify-self-center px-8 text-center text-4xl leading-snug! font-semibold md:text-5xl lg:text-6xl"
 	>
 		The
-		<strong class="rounded-2xl bg-brand-gradient px-5 py-0 text-black">truly</strong>
+		<strong class="bg-brand-gradient rounded-2xl px-5 py-0 text-black">truly</strong>
 		open&nbsp;source<br />
 		search
 		<span class="whitespace-nowrap"
@@ -17,8 +17,8 @@
 		>
 	</h2>
 	<p class="mb-8 max-w-full px-6 text-center text-sm sm:text-base md:max-w-184">
-		Mwmbl (pronounced "mumble") is a search engine that's completely independent, open
-		source, and non-profit.
+		Mwmbl (pronounced "mumble") is a search engine that's completely independent, open source, and
+		non-profit.
 	</p>
 	<Search />
 </main>
